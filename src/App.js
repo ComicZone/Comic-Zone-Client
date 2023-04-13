05db0fc9d4638fb234419a87efe55b81bf716fe8
Passion-cyber <chineduibelachi@gmail.com>
2023-04-13 22:23:16 +0100

    Started the categpries section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
 import Navbar from "./Components/Navbar";
+
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Navbar/>
         <Routes>
-          <Route path="/" />
-          <Route path="/main" />
+          <Route path="/" element={<Homepage />} />
         </Routes>
       </Router>
     </div>
