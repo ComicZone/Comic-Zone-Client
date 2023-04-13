00fd8d5a8e8ab3c5b57d69a80cce0dd5fd3dd4e9
deedyy <patience.udeh.234064.unn.edu.ng>
2023-04-13 16:43:40 +0100

    Navbr WIP

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Navbar from './Components/Navbar'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar/>
-       <Routes>
-        <Route path="/"/>
-         <Route path="/main"/>
-
-         
-
-       </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/main" />
+        </Routes>
       </Router>
-      
     </div>
   );
 }
