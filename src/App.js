3ae3053d79e859fcfe177b8df7b693294cff8cce
Marchukwu <nwekeamarachukwu28@gmail.com>
2023-04-13 16:17:08 +0100

    preview component started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
