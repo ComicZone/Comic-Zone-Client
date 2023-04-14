c98b0d0a9a1c04394c0d5276b2baf19db97024a5
iamifechi <iamifechi@gmail.com>
2023-04-14 15:55:48 +0100

    Done with 404 component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Navbar from "./Components/Navbar";
+// import Footer from "./Components/Footer";
+import Error from "./Components/404";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+        <Error/>
         <Routes>
           <Route path="/" element={<Homepage />} />
         </Routes>
+       
       </Router>
     </div>
   );
