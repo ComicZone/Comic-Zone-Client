e4442ab0bfaa932e8a6e2e000415585e4bc8650b
Passion-cyber <chineduibelachi@gmail.com>
2023-04-14 08:51:31 +0100

    completed the banner

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Banner from "../Components/Banner";
 // import CheckoutButton from "../Components/CheckoutButton";
-import Comics from "../Components/Comics";
 
 const Homepage = () => {
   return (
     <main>
       <Banner />
       {/* <CheckoutButton amount={'5.99'}/> */}
-      <Comics />
     </main>
   );
 };
