bc4e15b19bd64c895c6969e95d43f38b3785c653
Marchukwu <nwekeamarachukwu28@gmail.com>
2023-04-14 08:10:30 +0100

    Preview page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Preview from "./Pages/Preview";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
           <Route path="/main" />
         </Routes>
       </Router>
+      <Preview/>
     </div>
   );
 }
