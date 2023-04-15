de32c67b773b28491afe80dee0ebdc1f5b3e5cc1
iamifechi <iamifechi@gmail.com>
2023-04-15 02:31:10 +0100

    footer

---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Profile = () =>{
+  return (
+    <Container>Profile</Container>
+  )
+}
+
+export default Profile
+
+const Container = styled.section``
