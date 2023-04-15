f973625e4975decfba3477aabdf47c7828cb7ebe
iamifechi <iamifechi@gmail.com>
2023-04-15 07:01:19 +0100

    added profile to gitignore

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import Sidebar from '../Components/Profile/Sidebar'
+import Dashboard from '../Components/Profile/Dashboard'
 
 const Profile = () =>{
   return (
-    <Container>Profile</Container>
+    <Container>
+        <Sidebar/>
+        <Dashboard/>
+    </Container>
   )
 }
 
 export default Profile
 
-const Container = styled.section``
+const Container = styled.section`
+height: 90vh;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+gap: 2em;
+`
