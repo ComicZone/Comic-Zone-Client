6d4d90a008708d10a51c66818fcaa17823f27842
jumiej <akinyodejummy177@gmail.com>
2023-04-15 12:12:54 +0100

    WIP HERO

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Navbar from "./Components/Navbar";
 import Preview from "./Pages/Preview";
+import Card from "./Components/Card";
+import Books from "./Components/Books";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+        <Card/>
+        <Books/>
         <Routes>
           <Route path="/" element={<Homepage />} />
         </Routes>
---
+++ src/Components/Books.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import "./Card.css";
+import { HiFire } from "react-icons/hi";
+
+export default function Books() {
+	return (
+		<div>
+			<div className="Down">
+
+      <div className="BookCard3">
+					<div className="middle">
+						<div className="information">
+							<div className="FirstContent">
+								<HiFire className="fire" />
+								<div className="FirstItem">
+									<p>
+										Peaceful <br /> Rath
+									</p>
+								</div>
+							</div>
+
+							<div className="SecondContent">
+								<p>
+									Price: <br /> $ 100
+								</p>
+							</div>
+
+							<div className="ThirdContent">
+								<button> BUY </button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+        <div className="BookCard">
+					<div className="middle">
+						<div className="information">
+							<div className="FirstContent">
+								<HiFire className="fire" />
+								<div className="FirstItem">
+									<p>
+										Peaceful <br /> Rath
+									</p>
+								</div>
+							</div>
+
+							<div className="SecondContent">
+								<p>
+									Price: <br /> $ 100
+								</p>
+							</div>
+
+							<div className="ThirdContent">
+								<button> BUY </button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+        
+        <div className="BookCard">
+					<div className="middle">
+						<div className="information">
+							<div className="FirstContent">
+								<HiFire className="fire" />
+								<div className="FirstItem">
+									<p>
+										Peaceful <br /> Rath
+									</p>
+								</div>
+							</div>
+
+							<div className="SecondContent">
+								<p>
+									Price: <br /> $ 100
+								</p>
+							</div>
+
+							<div className="ThirdContent">
+								<button> BUY </button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+        <div className="BookCard">
+					<div className="middle">
+						<div className="information">
+							<div className="FirstContent">
+								<HiFire className="fire" />
+								<div className="FirstItem">
+									<p>
+										Peaceful <br /> Rath
+									</p>
+								</div>
+							</div>
+
+							<div className="SecondContent">
+								<p>
+									Price: <br /> $ 100
+								</p>
+							</div>
+
+							<div className="ThirdContent">
+								<button> BUY </button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+      </div>
+		</div>
+	);
+}
