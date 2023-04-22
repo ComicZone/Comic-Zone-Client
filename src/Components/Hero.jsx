a8a1f1540de39dc680f052dfc2653abebccdf94a
Passion-cyber <chineduibelachi@gmail.com>
2023-04-22 20:41:23 +0100

    Fixed the hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Card from "./Card";
-import Books from "./Books";
-import Banner from './Banner'
-import styled from "styled-components";
+import "../Stylesheets/Hero.css";
+// import Card from "./Card";
+// import Books from "./Books";
+// import Banner from "./Banner";
+// import styled from "styled-components";
 
 const Hero = () => {
   return (
-    <Section>
-      <Card/>
-      <Books/>
-      <Banner />
-    </Section>
+    <section className="hero-container">
+      <h1 className="hero-lead-text">
+        Heroes read <br /> about heroes
+      </h1>
+      <p className="hero-sub-text">
+        Digital marketplace for all kind of comic books, easily find and read{" "}
+        <br />
+        your favorite comic hero's
+      </p>
 
+      <Link to="/" className="hero-button">
+        View by Category
+      </Link>
+      {/* <div className="comic">
+      <Link to="/" className="hero-button">
+        View by Category
+      </Link>
+      </div> */}
+    </section>
   );
 };
 
 export default Hero;
 
-const Section = styled.section`
-`
+// const Section = styled.section``;
